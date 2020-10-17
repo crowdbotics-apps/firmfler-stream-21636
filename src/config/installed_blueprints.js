e5bb2b1e8a9be1330cf96d464a3711a842f5e18a
Crowdbotics <team@crowdbotics.com>
2020-10-17 22:56:01 +0000

    remove package blueprint-react-native-settings-screen

--- src/config/installed_blueprints.js
+++ src/config/installed_blueprints.js
@@ -2,7 +2,6 @@ export const installed_blueprints = [
   //@BlueprintInsertion
 { name: 'Dashboard225147132', human_name: 'Dashboard 2 25', access_route: 'Dashboard225147132'},
 { name: 'EmailAuth26147131', human_name: 'Email Auth 26', access_route: 'EmailAuth26147131', icon: 'envelope-o'},
-{ name: 'Settings27147130', human_name: 'Settings 27', access_route: 'Settings27147130'},
 { name: 'SignUp128147129', human_name: 'Sign Up 1 28', access_route: 'SignUp128147129'},
 { name: 'SignIn21146729', human_name: 'Sign In 2 1', access_route: 'SignIn21146729'},
 { name: 'UserProfile2146728', human_name: 'User Profile 2', access_route: 'UserProfile2146728'},
--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -7,7 +7,6 @@ import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import Dashboard225147132Navigator from '../features/Dashboard225147132/navigator';
 import EmailAuth26147131Navigator from '../features/EmailAuth26147131/navigator';
-import Settings27147130Navigator from '../features/Settings27147130/navigator';
 import SignUp128147129Navigator from '../features/SignUp128147129/navigator';
 import SignIn21146729Navigator from '../features/SignIn21146729/navigator';
 import UserProfile2146728Navigator from '../features/UserProfile2146728/navigator';
@@ -24,7 +23,6 @@ const AppNavigator = {
     //@BlueprintNavigationInsertion
 Dashboard225147132: { screen: Dashboard225147132Navigator },
 EmailAuth26147131: { screen: EmailAuth26147131Navigator },
-Settings27147130: { screen: Settings27147130Navigator },
 SignUp128147129: { screen: SignUp128147129Navigator },
 SignIn21146729: { screen: SignIn21146729Navigator },
 UserProfile2146728: { screen: UserProfile2146728Navigator },
