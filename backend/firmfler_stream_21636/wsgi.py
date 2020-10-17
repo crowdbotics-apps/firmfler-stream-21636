86a91e5fe9809200c95be921407cfaff465de5c6
Crowdbotics <team@crowdbotics.com>
2020-10-17 19:43:32 +0000

    Update models

--- backend/firmfler_stream_21636/wsgi.py
+++ backend/firmfler_stream_21636/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'firmfler_stream_21636.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "firmfler_stream_21636.settings")
 
 application = get_wsgi_application()
